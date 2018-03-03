76f207ce1cad8ea358b23947af1d8275df5c869f
lwkuant <xniber@gmail.com>
2018-03-03 23:49:49 +0800

    Try two more models

--- Modeling.py
+++ Modeling.py
@@ -110,14 +110,14 @@ print(time() - start_time)
 train_features = np.reshape(train_features, (num_train, np.prod(final_layer_shape)))
 validation_features = np.reshape(validation_features, (num_val, np.prod(final_layer_shape)))
 
-with open('./train_features', 'wb') as fp:
-    pickle.dump(train_features, fp)
-with open('./train_labels', 'wb') as fp:
-    pickle.dump(train_labels, fp)
-with open('./validation_features', 'wb') as fp:
-    pickle.dump(validation_features, fp)
-with open('./validation_labels', 'wb') as fp:
-    pickle.dump(validation_labels, fp)    
+#with open('./train_features', 'wb') as fp:
+#    pickle.dump(train_features, fp)
+#with open('./train_labels', 'wb') as fp:
+#    pickle.dump(train_labels, fp)
+#with open('./validation_features', 'wb') as fp:
+#    pickle.dump(validation_features, fp)
+#with open('./validation_labels', 'wb') as fp:
+#    pickle.dump(validation_labels, fp)    
 
 with open('./train_features', 'rb') as fp:
     train_features = pickle.load(fp)
@@ -136,7 +136,7 @@ from keras import optimizers
 np.random.seed(seed)
 model = models.Sequential()
 model.add(layers.Dense(256, activation='relu', input_dim=np.prod(final_layer_shape)))
-model.add(layers.Dropout(0.5))
+#model.add(layers.Dropout(0.5))
 model.add(layers.Dense(num_class, activation='softmax'))
 model.compile(optimizer=optimizers.RMSprop(lr=2e-5),
     loss='categorical_crossentropy',
@@ -185,6 +185,8 @@ plt.savefig('Loss_VGG19_1_layer_256.png')
 ###
 
 batch_size = 1
+times_augmentation = 4
+
 from keras.preprocessing.image import ImageDataGenerator
 from keras.applications import VGG19
 
@@ -195,6 +197,7 @@ conv_base = VGG19(weights='imagenet',
 conv_base.summary() # shape of last layer = (6, 8, 512) 
 final_layer_shape = [6, 8, 512]
 
+'''
 datagen = ImageDataGenerator(rescale=1./255)
 
 datagen_augmentation = ImageDataGenerator(
@@ -237,12 +240,230 @@ def extract_features_with_augmentation(directory, sample_count, batch_size, time
     return features, labels
 
 
+
 start_time = time()
 train_features, train_labels = extract_features_with_augmentation(train_dir, num_train, batch_size,
-                                                4) # 
+                                                times_augmentation) # 6035.708188533783s
 print(time() - start_time)
+'''
+
+#with open('./train_features_augmentation_40k', 'wb') as fp:
+#    pickle.dump(train_features, fp, protocol=4)
+#with open('./train_labels_augmentation_40k', 'wb') as fp:
+#    pickle.dump(train_labels, fp)
+
+with open('./train_features_augmentation_40k', 'rb') as fp:
+    train_features = pickle.load(fp)
+with open('./train_labels_augmentation_40k', 'rb') as fp:
+    train_labels = pickle.load(fp)
 
 with open('./validation_features', 'rb') as fp:
     validation_features = pickle.load(fp)
 with open('./validation_labels', 'rb') as fp:
     validation_labels = pickle.load(fp)    
+    
+np.random.seed(seed)
+augmentation_ind = np.random.choice(np.arange(num_train*times_augmentation),
+                                    num_train*times_augmentation, replace = False)
+
+train_features = train_features[augmentation_ind]
+train_labels = train_labels[augmentation_ind]
+
+train_features = np.reshape(train_features, (num_train*times_augmentation, np.prod(final_layer_shape)))
+validation_features = np.reshape(validation_features, (num_val, np.prod(final_layer_shape)))
+
+
+from keras import models
+from keras import layers
+from keras import optimizers
+
+np.random.seed(seed)
+model = models.Sequential()
+model.add(layers.Dense(256, activation='relu', input_dim=np.prod(final_layer_shape)))
+#model.add(layers.Dropout(0.5))
+model.add(layers.Dense(num_class, activation='softmax'))
+model.compile(optimizer=optimizers.RMSprop(lr=2e-5),
+    loss='categorical_crossentropy',
+    metrics=['acc'])
+history = model.fit(train_features, train_labels,
+    epochs=50,
+    batch_size=20,
+    validation_data=(validation_features, validation_labels))
+
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs = range(1, len(acc) + 1)
+
+sns.set_style("darkgrid")
+plt.figure(figsize=[20, 15])
+plt.rcParams['xtick.labelsize'] = 15
+plt.rcParams['ytick.labelsize'] = 15
+plt.plot(epochs, acc, 'ko', label='Training Accuracy')
+plt.plot(epochs, val_acc, 'k', label='Validation Accuracy')
+plt.yticks(np.linspace(0, 1, 6))
+plt.xticks(np.linspace(0, 50, 6))
+plt.title('Training and Validation Accuracy', fontsize=25)
+plt.xlabel('Epochs', fontsize=20)
+plt.ylabel('Accuracy', fontsize=20)
+plt.legend(loc=4, prop={'size': 15})
+plt.savefig('Accuracy_VGG19_Data_Augmentation_40k_1_layer_256.png')
+
+sns.set_style("darkgrid")
+plt.figure(figsize=[20, 15])
+plt.rcParams['xtick.labelsize'] = 15
+plt.rcParams['ytick.labelsize'] = 15
+plt.plot(epochs, loss, 'ko', label='Training Loss')
+plt.plot(epochs, val_loss, 'k', label='Validation Loss')
+plt.title('Training and Validation Loss', fontsize=25)
+plt.xlabel('Epochs', fontsize=20)
+plt.ylabel('Accuracy', fontsize=20)
+plt.legend(loc=0, prop={'size': 15})
+plt.savefig('Loss_VGG19_Data_Augmentation_40k_1_layer_256.png')
+
+
+###
+### Pretrained Model (VGG19) with Retraining the Block5 in VGG19
+###
+
+###
+batch_size = 1
+from keras.preprocessing.image import ImageDataGenerator
+from keras.applications import VGG19
+
+conv_base = VGG19(weights='imagenet',
+                  include_top=False,
+                  input_shape=(height, width, 3))
+
+conv_base_raw = VGG19(weights='imagenet',
+                  include_top=False,
+                  input_shape=(height, width, 3))
+
+for i in range(5):
+    conv_base.layers.pop()
+
+conv_base.outputs = [conv_base.layers[-1].output]
+conv_base.layers[-1].outbound_nodes = []
+
+conv_base.summary() # shape of last layer = (12, 16, 512) 
+final_layer_shape = [12, 16, 512]
+
+datagen = ImageDataGenerator(rescale=1./255)
+
+def extract_features(directory, sample_count, batch_size):
+    features = np.zeros(shape=([sample_count] + final_layer_shape))
+    labels = np.zeros(shape=(sample_count, num_class))
+    generator = datagen.flow_from_directory(
+        directory,
+        target_size=(height, width),
+        batch_size=batch_size,
+        class_mode='categorical')
+    i = 0
+    for inputs_batch, labels_batch in generator:
+        features_batch = conv_base.predict(inputs_batch)
+        features[i * batch_size : (i + 1) * batch_size] = features_batch
+        labels[i * batch_size : (i + 1) * batch_size] = labels_batch
+        i += 1
+        print(i)
+        if i * batch_size >= sample_count:
+            break
+    return features, labels
+
+start_time = time()
+train_features, train_labels = extract_features(train_dir, num_train, batch_size) # 1289.7562346458435s
+print(time() - start_time)
+
+start_time = time()
+validation_features, validation_labels = extract_features(val_dir, num_val, 1) # 257.6586627960205s
+print(time() - start_time)
+
+#train_features = np.reshape(train_features, (num_train, np.prod(final_layer_shape)))
+#validation_features = np.reshape(validation_features, (num_val, np.prod(final_layer_shape)))
+
+#with open('./train_features_without_block5', 'wb') as fp:
+#    pickle.dump(train_features, fp, protocol = 4)
+#with open('./train_labels_without_block5', 'wb') as fp:
+#    pickle.dump(train_labels, fp)
+#with open('./validation_features_without_block5', 'wb') as fp:
+#    pickle.dump(validation_features, fp)
+#with open('./validation_labels_without_block5', 'wb') as fp:
+#    pickle.dump(validation_labels, fp)    
+    
+with open('./train_features_without_block5', 'rb') as fp:
+    train_features = pickle.load(fp)
+with open('./train_labels_without_block5', 'rb') as fp:
+    train_labels = pickle.load(fp)
+with open('./validation_features_without_block5', 'rb') as fp:
+    validation_features = pickle.load(fp)
+with open('./validation_labels_without_block5', 'rb') as fp:
+    validation_labels = pickle.load(fp)    
+
+
+from keras import models
+from keras import layers
+from keras import optimizers
+
+#from keras.callbacks import EarlyStopping
+#early_stopping_monitor = EarlyStopping(monitor = 'acc', patience=5)
+
+np.random.seed(seed)
+model = models.Sequential()
+model.add(layers.ZeroPadding2D((1,1), input_shape=(12, 16, 512)))
+model.add(layers.Conv2D(512, (3, 3), activation='relu'))
+model.add(layers.ZeroPadding2D((1,1)))
+model.add(layers.Conv2D(512, (3, 3), activation='relu'))
+model.add(layers.ZeroPadding2D((1,1)))
+model.add(layers.Conv2D(512, (3, 3), activation='relu'))
+model.add(layers.ZeroPadding2D((1,1)))
+model.add(layers.Conv2D(512, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2,2), strides=(2,2)))
+model.add(layers.Flatten())
+model.add(layers.Dense(256, activation='relu', input_dim=np.prod(final_layer_shape)))
+#model.add(layers.Dropout(0.5))
+model.add(layers.Dense(num_class, activation='softmax'))
+model.compile(optimizer=optimizers.RMSprop(lr=2e-5),
+    loss='categorical_crossentropy',
+    metrics=['acc'])
+history = model.fit(train_features, train_labels,
+    epochs=30,
+    batch_size=20,
+    validation_data=(validation_features, validation_labels))
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs = range(1, len(acc) + 1)
+
+sns.set_style("darkgrid")
+plt.figure(figsize=[20, 15])
+plt.rcParams['xtick.labelsize'] = 15
+plt.rcParams['ytick.labelsize'] = 15
+plt.plot(epochs, acc, 'ko', label='Training Accuracy')
+plt.plot(epochs, val_acc, 'k', label='Validation Accuracy')
+plt.yticks(np.linspace(0, 1, 6))
+plt.xticks(np.linspace(0, 30, 4))
+plt.title('Training and Validation Accuracy', fontsize=25)
+plt.xlabel('Epochs', fontsize=20)
+plt.ylabel('Accuracy', fontsize=20)
+plt.legend(loc=4, prop={'size': 15})
+plt.savefig('Accuracy_VGG19_Retraining_Block5_1_layer_256.png')
+
+sns.set_style("darkgrid")
+plt.figure(figsize=[20, 15])
+plt.rcParams['xtick.labelsize'] = 15
+plt.rcParams['ytick.labelsize'] = 15
+plt.plot(epochs, loss, 'ko', label='Training Loss')
+plt.plot(epochs, val_loss, 'k', label='Validation Loss')
+plt.xticks(np.linspace(0, 30, 4))
+plt.title('Training and Validation Loss', fontsize=25)
+plt.xlabel('Epochs', fontsize=20)
+plt.ylabel('Accuracy', fontsize=20)
+plt.legend(loc=0, prop={'size': 15})
+plt.savefig('Loss_VGG19_Retraining_Block5_1_layer_256.png')
--- README.md
+++ README.md
@@ -4,16 +4,26 @@
 This project aims to build a deep learning model to accurately classify 55 different food with various sizes. For more information, please refer to this [site](https://deepanalytics.jp/compe/59)
 
 ## Data 
-1. Number of training set(.jpg): 11,995
+1. Number of training set(.jpg): 11,995 (Training: 10,000 & Validation: 1,995)
 2. Number of test set(.jpg): 3,937
 3. Number of classes: 55
 
 ## Methology 
 1. Transform the data with various sizes to (192, 256, 3)
-2. Use VGG19 pretrained for feature extraction
+2. Use VGG19 pretrained model for feature extraction
 3. Implement Data augmentation
+4. Retrain part of the layers of the VGG19 pretrained model
 
 ## Result
 
+### VGG19 Pretrained Model + 1 Layer Dense Model (256, ) 
 <img src="https://github.com/lwkuant/Side_project_Cookpad_Food_Classification/blob/master/Accuracy_VGG19_1_layer_256.png">
 <img src="https://github.com/lwkuant/Side_project_Cookpad_Food_Classification/blob/master/Loss_VGG19_1_layer_256.png">
+
+### VGG19 Pretrained Model + 1 Layer Dense Model (256, ) + Data Augmentation to 40,000 pictures
+<img src="https://github.com/lwkuant/Side_project_Cookpad_Food_Classification/blob/master/Accuracy_VGG19_Data_Augmentation_40k_1_layer_256.png">
+<img src="https://github.com/lwkuant/Side_project_Cookpad_Food_Classification/blob/master/Loss_VGG19_Data_Augmentation_40k_1_layer_256.png">
+
+### VGG19 Pretrained Model + Retrain Block 5 of VGG19 + 1 Layer Dense Model (256, )
+<img src="https://github.com/lwkuant/Side_project_Cookpad_Food_Classification/blob/master/Accuracy_VGG19_Retraining_Block5_1_layer_256.png">
+<img src="https://github.com/lwkuant/Side_project_Cookpad_Food_Classification/blob/master/Loss_VGG19_Retraining_Block5_1_layer_256.png">
