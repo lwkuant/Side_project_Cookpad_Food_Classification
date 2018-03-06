ef36c6f5226c0a68478b00cc55d75d84b240138e
lwkuant <xniber@gmail.com>
2018-03-06 23:53:56 +0800

    Use InceptionResNetV2 pretrained model

--- Modeling.py
+++ Modeling.py
@@ -519,19 +519,19 @@ validation_features = np.reshape(validation_features, (num_val, np.prod(final_la
 #    pickle.dump(train_features, fp)
 #with open('./train_labels', 'wb') as fp:
 #    pickle.dump(train_labels, fp)
-#with open('./validation_features', 'wb') as fp:
-#    pickle.dump(validation_features, fp)
-#with open('./validation_labels', 'wb') as fp:
-#    pickle.dump(validation_labels, fp)    
+with open('./validation_features_InceptionV3', 'wb') as fp:
+    pickle.dump(validation_features, fp)
+with open('./validation_labels', 'wb') as fp:
+    pickle.dump(validation_labels, fp)    
 
-with open('./train_features', 'rb') as fp:
-    train_features = pickle.load(fp)
-with open('./train_labels', 'rb') as fp:
-    train_labels = pickle.load(fp)
-with open('./validation_features', 'rb') as fp:
-    validation_features = pickle.load(fp)
-with open('./validation_labels', 'rb') as fp:
-    validation_labels = pickle.load(fp)    
+#with open('./train_features', 'rb') as fp:
+#    train_features = pickle.load(fp)
+#with open('./train_labels', 'rb') as fp:
+#    train_labels = pickle.load(fp)
+#with open('./validation_features', 'rb') as fp:
+#    validation_features = pickle.load(fp)
+#with open('./validation_labels', 'rb') as fp:
+#    validation_labels = pickle.load(fp)    
 
 
 from keras import models
@@ -601,7 +601,7 @@ conv_base = InceptionV3(weights='imagenet',
                   input_shape=(height, width, 3))
 
 conv_base.summary() # shape of last layer = (6, 8, 512) 
-final_layer_shape = [6, 8, 512]
+final_layer_shape = [4, 6, 2048]
 
 
 datagen = ImageDataGenerator(rescale=1./255)
@@ -649,14 +649,14 @@ def extract_features_with_augmentation(directory, sample_count, batch_size, time
 
 start_time = time()
 train_features, train_labels = extract_features_with_augmentation(train_dir, num_train, batch_size,
-                                                times_augmentation) # 6035.708188533783s
+                                                times_augmentation) # 4121.0559158325195s
 print(time() - start_time)
 
 
-#with open('./train_features_augmentation_40k', 'wb') as fp:
-#    pickle.dump(train_features, fp, protocol=4)
-#with open('./train_labels_augmentation_40k', 'wb') as fp:
-#    pickle.dump(train_labels, fp)
+with open('./train_features_augmentation_40k_InceptionV3', 'wb') as fp:
+    pickle.dump(train_features, fp, protocol=4)
+with open('./train_labels_augmentation_40k_InceptionV3', 'wb') as fp:
+    pickle.dump(train_labels, fp)
 
 with open('./train_features_augmentation_40k', 'rb') as fp:
     train_features = pickle.load(fp)
@@ -730,3 +730,121 @@ plt.xlabel('Epochs', fontsize=20)
 plt.ylabel('Accuracy', fontsize=20)
 plt.legend(loc=0, prop={'size': 15})
 plt.savefig('Loss_VGG19_Data_Augmentation_40k_1_layer_256.png')
+
+
+###
+### Pretrained Model (InceptionResNetV2)
+###
+np.random.seed(seed)
+batch_size = 1
+from keras import models
+from keras import layers
+from keras import optimizers
+from keras.preprocessing.image import ImageDataGenerator
+from keras.applications import InceptionResNetV2
+
+conv_base = InceptionResNetV2(weights='imagenet',
+                  include_top=False,
+                  input_shape=(height, width, 3))
+
+conv_base.summary() # shape of last layer = (6, 8, 512) 
+final_layer_shape = [4, 6, 1536]
+
+datagen = ImageDataGenerator(rescale=1./255)
+
+def extract_features(directory, sample_count, batch_size):
+    features = np.zeros(shape=([sample_count] + final_layer_shape))
+    labels = np.zeros(shape=(sample_count, num_class))
+    generator = datagen.flow_from_directory(
+        directory,
+        target_size=(height, width),
+        batch_size=batch_size,
+        class_mode='categorical')
+    i = 0
+    for inputs_batch, labels_batch in generator:
+        features_batch = conv_base.predict(inputs_batch)
+        features[i * batch_size : (i + 1) * batch_size] = features_batch
+        labels[i * batch_size : (i + 1) * batch_size] = labels_batch
+        i += 1
+        print(i)
+        if i * batch_size >= sample_count:
+            break
+    return features, labels
+
+start_time = time()
+train_features, train_labels = extract_features(train_dir, num_train, batch_size) # 
+print(time() - start_time)
+
+start_time = time()
+validation_features, validation_labels = extract_features(val_dir, num_val, 1) # Since the number of validation set cannot be divided completely by 4
+print(time() - start_time)
+
+train_features = np.reshape(train_features, (num_train, np.prod(final_layer_shape)))
+validation_features = np.reshape(validation_features, (num_val, np.prod(final_layer_shape)))
+
+with open('./train_features_InceptionResNetV2', 'wb') as fp:
+    pickle.dump(train_features, fp)
+with open('./train_labels_InceptionResNetV2', 'wb') as fp:
+    pickle.dump(train_labels, fp)
+with open('./validation_features_InceptionResNetV2', 'wb') as fp:
+    pickle.dump(validation_features, fp)
+with open('./validation_InceptionResNetV2', 'wb') as fp:
+    pickle.dump(validation_labels, fp)    
+
+#with open('./train_features', 'rb') as fp:
+#    train_features = pickle.load(fp)
+#with open('./train_labels', 'rb') as fp:
+#    train_labels = pickle.load(fp)
+#with open('./validation_features', 'rb') as fp:
+#    validation_features = pickle.load(fp)
+#with open('./validation_labels', 'rb') as fp:
+#    validation_labels = pickle.load(fp)    
+
+
+###
+np.random.seed(seed)
+model = models.Sequential()
+model.add(layers.Dense(256, activation='relu', input_dim=np.prod(final_layer_shape)))
+#model.add(layers.Dropout(0.5))
+model.add(layers.Dense(num_class, activation='softmax'))
+model.compile(optimizer=optimizers.RMSprop(lr=2e-5),
+    loss='categorical_crossentropy',
+    metrics=['acc'])
+history = model.fit(train_features, train_labels,
+    epochs=50,
+    batch_size=20,
+    validation_data=(validation_features, validation_labels))
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs = range(1, len(acc) + 1)
+
+sns.set_style("darkgrid")
+plt.figure(figsize=[20, 15])
+plt.rcParams['xtick.labelsize'] = 15
+plt.rcParams['ytick.labelsize'] = 15
+plt.plot(epochs, acc, 'ko', label='Training Accuracy')
+plt.plot(epochs, val_acc, 'k', label='Validation Accuracy')
+plt.yticks(np.linspace(0, 1, 6))
+plt.xticks(np.linspace(0, 50, 6))
+plt.title('Training and Validation Accuracy', fontsize=25)
+plt.xlabel('Epochs', fontsize=20)
+plt.ylabel('Accuracy', fontsize=20)
+plt.legend(loc=4, prop={'size': 15})
+plt.savefig('Accuracy_InceptionResNetV2_1_layer_256.png')
+
+sns.set_style("darkgrid")
+plt.figure(figsize=[20, 15])
+plt.rcParams['xtick.labelsize'] = 15
+plt.rcParams['ytick.labelsize'] = 15
+plt.plot(epochs, loss, 'ko', label='Training Loss')
+plt.plot(epochs, val_loss, 'k', label='Validation Loss')
+plt.title('Training and Validation Loss', fontsize=25)
+plt.xlabel('Epochs', fontsize=20)
+plt.ylabel('Accuracy', fontsize=20)
+plt.legend(loc=0, prop={'size': 15})
+plt.savefig('Loss_InceptionResNetV2_1_layer_256.png')
--- README.md
+++ README.md
@@ -13,23 +13,30 @@ This project aims to build a deep learning model to accurately classify 55 diffe
 2. Use pretrained model for feature extraction:
 	(1) VGG19
 	(2) InceptionV3
+	(3) InceptionResNetV2
 3. Implement Data augmentation
 4. Retrain part of the layers of the VGG19 pretrained model
 
 ## Result
 
 ### VGG19 Pretrained Model + 1 Layer Dense Model (256, ) 
-<img src="https://github.com/lwkuant/Side_project_Cookpad_Food_Classification/blob/master/Accuracy_VGG19_1_layer_256.png">
-<img src="https://github.com/lwkuant/Side_project_Cookpad_Food_Classification/blob/master/Loss_VGG19_1_layer_256.png">
+<img src="https://github.com/lwkuant/Side_project_Cookpad_Food_Classification/blob/master/Image/Accuracy_VGG19_1_layer_256.png">
+<img src="https://github.com/lwkuant/Side_project_Cookpad_Food_Classification/blob/master/Image/Loss_VGG19_1_layer_256.png">
 
 ### VGG19 Pretrained Model + 1 Layer Dense Model (256, ) + Data Augmentation to 40,000 pictures
-<img src="https://github.com/lwkuant/Side_project_Cookpad_Food_Classification/blob/master/Accuracy_VGG19_Data_Augmentation_40k_1_layer_256.png">
-<img src="https://github.com/lwkuant/Side_project_Cookpad_Food_Classification/blob/master/Loss_VGG19_Data_Augmentation_40k_1_layer_256.png">
+<img src="https://github.com/lwkuant/Side_project_Cookpad_Food_Classification/blob/master/Image/Accuracy_VGG19_Data_Augmentation_40k_1_layer_256.png">
+<img src="https://github.com/lwkuant/Side_project_Cookpad_Food_Classification/blob/master/Image/Loss_VGG19_Data_Augmentation_40k_1_layer_256.png">
 
 ### VGG19 Pretrained Model + Retrain Block 5 of VGG19 + 1 Layer Dense Model (256, )
-<img src="https://github.com/lwkuant/Side_project_Cookpad_Food_Classification/blob/master/Accuracy_VGG19_Retraining_Block5_1_layer_256.png">
-<img src="https://github.com/lwkuant/Side_project_Cookpad_Food_Classification/blob/master/Loss_VGG19_Retraining_Block5_1_layer_256.png">
+<img src="https://github.com/lwkuant/Side_project_Cookpad_Food_Classification/blob/master/Image/Accuracy_VGG19_Retraining_Block5_1_layer_256.png">
+<img src="https://github.com/lwkuant/Side_project_Cookpad_Food_Classification/blob/master/Image/Loss_VGG19_Retraining_Block5_1_layer_256.png">
 
 ### InceptionV3 Pretrained Model + 1 Layer Dense Model (256, ) 
-<img src="https://github.com/lwkuant/Side_project_Cookpad_Food_Classification/blob/master/Accuracy_InceptionV3_1_layer_256.png">
-<img src="https://github.com/lwkuant/Side_project_Cookpad_Food_Classification/blob/master/Loss_InceptionV3_1_layer_256.png">
+<img src="https://github.com/lwkuant/Side_project_Cookpad_Food_Classification/blob/master/Image/Accuracy_InceptionV3_1_layer_256.png">
+<img src="https://github.com/lwkuant/Side_project_Cookpad_Food_Classification/blob/master/Image/Loss_InceptionV3_1_layer_256.png">
+
+### InceptionResNetV2 Pretrained Model + 1 Layer Dense Model (256, ) 
+<img src="https://github.com/lwkuant/Side_project_Cookpad_Food_Classification/blob/master/Image/Accuracy_InceptionResNetV2_1_layer_256.png">
+<img src="https://github.com/lwkuant/Side_project_Cookpad_Food_Classification/blob/master/Image/Loss_InceptionResNetV2_1_layer_256.png">
+
+
